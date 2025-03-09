4d55a4c170cfad03291526d7dcfbe84bf497a238
unknown <pandi14796@gmail.com>
2025-03-09 19:50:17 +0530

    updated swagger

--- src/main.ts
+++ src/main.ts
@@ -1,17 +1,26 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
-import { GlobalExceptionFilter } from './filters/http-exception.filter';
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
   app.enableCors();
-  app.useGlobalPipes(new ValidationPipe({ whitelist: true }));
-  app.useGlobalFilters(new GlobalExceptionFilter()); // Use Global Exception Filter
+  app.useGlobalPipes(new ValidationPipe());
+
+  // Swagger Configuration
+  const config = new DocumentBuilder()
+    .setTitle('User Management API')
+    .setDescription('API documentation for authentication and user management')
+    .setVersion('1.0')
+    .addBearerAuth()
+    .build();
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, document);
 
   await app.listen(3000);
-  console.log('Server running on http://localhost:3000');
+  console.log('Swagger API Docs available at http://localhost:3000/api');
 }
 
 bootstrap();
