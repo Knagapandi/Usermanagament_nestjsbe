5f33b8fbc67ce1c3c2b335789c083c929acc91d8
unknown <pandi14796@gmail.com>
2025-03-09 19:45:09 +0530

    updated test case

--- src/main.ts
+++ src/main.ts
@@ -1,12 +1,14 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
+import { GlobalExceptionFilter } from './filters/http-exception.filter';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
   app.enableCors();
-  app.useGlobalPipes(new ValidationPipe());
+  app.useGlobalPipes(new ValidationPipe({ whitelist: true }));
+  app.useGlobalFilters(new GlobalExceptionFilter()); // Use Global Exception Filter
 
   await app.listen(3000);
   console.log('Server running on http://localhost:3000');
